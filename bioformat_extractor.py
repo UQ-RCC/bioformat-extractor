f06fb52cb0eee0f3dec4db5bab44e4c51ea8c6fc
Hoang Nguyen <hoangnguyen177@hotmail.com>
2020-09-10 00:18:02 +1000

    now test

--- bioformat_extractor.py
+++ bioformat_extractor.py
@@ -2,9 +2,9 @@ import pyclowder
 import re
 import logging
 import tempfile
-import os
+import os, json
 import subprocess
-
+import csv
 from pyclowder.extractors import Extractor
 import pyclowder.files
 
@@ -25,12 +25,13 @@ class BioformatExtractor(Extractor):
 
         # parse command line and load default logging configuration
         self.setup()
-
+        self.bioformat_extensions = get_good_bioformats()
         # setup logging for the exctractor
         logging.getLogger('pyclowder').setLevel(logging.DEBUG)
         logging.getLogger('__main__').setLevel(logging.DEBUG)
 
-    def get_info(self, obj):
+    @staticmethod
+    def get_info(obj):
         attr = dict()
         type_name = type(obj).__name__
         prop_names = dir(obj)
@@ -45,6 +46,24 @@ class BioformatExtractor(Extractor):
             except:
                 pass
         return attr
+    
+    @staticmethod
+    def get_good_bioformats():
+        l = dict()
+        with open("bioformats.tsv") as fd:
+            rd = csv.reader(fd, delimiter="\t", quotechar='"')
+            next(rd, None) # skip header
+            for row in rd:
+                _format = row[0]
+                _extensions = row[1]
+                _pixels_quality = int(row[2].split('-')[0].strip())
+                _metadata_quality = int(row[3].split('-')[0].strip())
+                if _pixels_quality >= 3 and _metadata_quality >=3:
+                    l[_format] = _extensions
+        extensions = []
+        for _value in l.values():
+            extensions.extend(_value.split(','))
+        return extensions
 
     def process_message(self, connector, host, secret_key, resource, parameters):
         # Process the file and upload the results
@@ -52,29 +71,32 @@ class BioformatExtractor(Extractor):
         logger = logging.getLogger(__name__)
         inputfile = resource["local_paths"][0]
         file_id = resource['id']
+        filename, file_extension = os.path.splitext(inputfile)
+        if file_extension in self.bioformat_extensions:
+            #this part handles the metadata
+            try:
+                result = dict()
+                omexmlstr=bioformats.get_omexml_metadata(inputfile)
+                o=bioformats.OMEXML(omexmlstr)
+                #
+                result['Name'] = o.get_Name()
+                result['AcquisitionDate'] = o.get_AcquisitionDate()
+                result['ID'] = o.get_ID()
+                result['ns'] = o.ns
+                # pixel infor
+                pixels=o.image().Pixels()
+                result['pixel'] = get_info(pixels)
 
-        #this part handles the metadata
-        try:
-            result = dict()
+                metadata = self.get_metadata(result, 'file', file_id, host)
+                logger.debug(metadata)
+                # upload metadata
+                pyclowder.files.upload_metadata(connector, host, secret_key, file_id, metadata)
 
-            omexmlstr=bioformats.get_omexml_metadata(inputfile)
-            o=bioformats.OMEXML(omexmlstr)
-            #
-            result['Name'] = o.get_Name()
-            result['AcquisitionDate'] = o.get_AcquisitionDate()
-            result['ID'] = o.get_ID()
-            result['ns'] = o.ns
-            # pixel infor
-            pixels=o.image().Pixels()
-            result['pixel'] = self.get_info(pixels)
-
-            metadata = self.get_metadata(result, 'file', file_id, host)
-            logger.debug(metadata)
-            # upload metadata
-            pyclowder.files.upload_metadata(connector, host, secret_key, file_id, metadata)
-
-        except Exception as e:
-            logger.error("Error getting metadata from file", e)
+            except Exception as e:
+                logger.error("Error getting metadata from file", e)
+        else:
+            logger.error(f"File format not supprted, Supported list:{self.bioformat_extensions}")
+        
 
 
 if __name__ == "__main__":
